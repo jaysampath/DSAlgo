85d7c0e6f3652398452f48c2d98846cbb64a532e
Jay <jayasampath111@gmail.com>
2022-08-21 18:06:47 +0530

    1ms solution

--- Minimum Recolors to Get K Consecutive Black Blocks/Solution.java
+++ Minimum Recolors to Get K Consecutive Black Blocks/Solution.java
@@ -2,23 +2,18 @@ class Solution {
     public int minimumRecolors(String blocks, int k) {
         int count=0;
         char[] ch = blocks.toCharArray();
-        for(int i=0;i<ch.length;i++){
-            if(count==k)
-                return 0;
-            if(ch[i]=='B')
-                count++;
-            else
-                count=0;
-        }
-
-        //already not present case
         int minCount =Integer.MAX_VALUE;
         for(int i=0;i<=ch.length-k;i++){
             count=0;
+            //iterate through k sized subarray from i
             for(int j=i;j<i+k;j++){
                 if(ch[j]=='W')
                     count++;
             }
+            //already present case.
+            if(count==0)
+                return 0;
+            //not present find the min
             if(count<minCount)
                 minCount=count;
         }
