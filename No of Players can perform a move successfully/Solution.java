8c1a472736fa6561b1a0bec31ce91c076d0d2810
Jay <jayasampath111@gmail.com>
2023-10-12 11:45:10 +0530

    Dynamic programming approach

--- No of Players can perform a move successfully/Solution.java
+++ No of Players can perform a move successfully/Solution.java
@@ -1,1 +1,32 @@
+import java.util.Arrays;
 
+public class Solution {
+    private int solution(String S) {
+        int n = S.length();
+        boolean[] positionOccupied = new boolean[n];
+        Arrays.fill(positionOccupied, true);
+        int count = 0;
+        for (int i = 0; i< n; i++) {
+            char move = S.charAt(i);
+            int targetPosition = i;
+            if (move == '<') {
+                targetPosition--;
+            } else if (move == '>') {
+                targetPosition++;
+            } else {
+                targetPosition = targetPosition - n -n;
+            }
+            if (targetPosition >= 0 && targetPosition < n) {
+               if (!positionOccupied[targetPosition]) {
+                   count++;
+                   positionOccupied[targetPosition] = false;
+               }
+            } else {
+                positionOccupied[i] = false;
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
